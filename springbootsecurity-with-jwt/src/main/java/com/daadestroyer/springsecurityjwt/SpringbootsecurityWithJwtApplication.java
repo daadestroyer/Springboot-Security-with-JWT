225fbeea3497e15da7ef401110f9b13c7fe5e191
daadestroyer <nigamshubham2000@gmail.com>
2022-10-09 18:55:16 +0530

    first commit

---
+++ springbootsecurity-with-jwt/src/main/java/com/daadestroyer/springsecurityjwt/SpringbootsecurityWithJwtApplication.java
@@ -0,0 +1,13 @@
+package com.daadestroyer.springsecurityjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootsecurityWithJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootsecurityWithJwtApplication.class, args);
+	}
+
+}
---
+++ springbootsecurity-with-jwt/src/test/java/com/daadestroyer/springsecurityjwt/SpringbootsecurityWithJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daadestroyer.springsecurityjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootsecurityWithJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
